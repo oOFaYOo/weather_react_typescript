e8b047af6d24e0dca4bfbf1bf9f66f8a368920ea
oOFaYOo <nika.tseleva@mail.ru>
2021-07-18 07:20:06 +0500

    added switcher of info of weather

--- my-app/src/components/Container/Week/Day/index.tsx
+++ my-app/src/components/Container/Week/Day/index.tsx
@@ -8,31 +8,55 @@ interface IDayProps {
     isFirst: boolean;
 }
 
-class Day extends PureComponent<IDayProps>{
+interface IDayState {
+    isMainInfoOpen: boolean;
+    isInfoOpen: boolean;
+    isDateOpen: boolean;
+}
+
+class Day extends PureComponent<IDayProps, IDayState>{
+
+    state = {
+        isMainInfoOpen: true,
+        isInfoOpen: false,
+        isDateOpen: true
+    }
+
     public render(): React.ReactNode {
         return (
             <div className={`days ${this.props.isFirst?"now":""}`} id={this.props.weather.id}>
                 <div className="main">
                     <div className="day">{this.props.weather.dayOfWeek}</div>
-                    <div className="main_info">
+                    <div className="main_info"
+                         onClick={this.switchesInfo}
+                         style={{display:`${this.state.isMainInfoOpen?"block":"none"}`}}>
                         <span className="max_day_temp">{this.props.weather.maxTemp}</span><br />
                         <span className="min_day_temp">{this.props.weather.minTemp}</span>
                     </div>
                 </div>
                 <div className="moreInfo">
                     <div className="info"
-                         // hidden="" style="display: none;"
-                    >
+                         onClick={this.switchesInfo}
+                         style={{display:`${this.state.isInfoOpen?"block":"none"}`}}>
                         <span className="morn_temp">Утро: {this.props.weather.morning}</span><br />
                         <span className="day_temp">День: {this.props.weather.day}</span><br />
                         <span className="ev_temp">Вечер: {this.props.weather.evening}</span><br />
                         <span className="night_temp">Ночь: {this.props.weather.night}</span>
                     </div><br />
-                    <div className="date">{this.props.weather.date}</div>
+                    <div className="date" style={{display:`${this.state.isDateOpen?"block":"none"}`}}>{this.props.weather.date}</div>
                 </div>
             </div>
         )
     }
+
+    switchesInfo = ():void => {
+        this.setState({
+            isMainInfoOpen: !this.state.isMainInfoOpen,
+            isInfoOpen: !this.state.isInfoOpen,
+            isDateOpen: !this.state.isDateOpen
+        })
+    }
+
 }
 
 export default Day;
